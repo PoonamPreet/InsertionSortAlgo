d2d65bc6d20b61187d994191e01d2ba99c37bfb6
Poonam <anmolpreetsmailbox@gmail.com>
2022-02-20 16:16:13 +0530

    Insertion Sort Algo

---
+++ src/bruteforcealgo/BruteForceAlgo.java
@@ -0,0 +1,33 @@
+package bruteforcealgo;
+
+public class BruteForceAlgo {
+
+	//we need not use obj in main class so static method is created
+	public static void insertionSort(int a1[])
+	{
+		for(int i=0;i<a1.length;i++)
+		{
+			int j=i-1;
+			int b=a1[i];
+			
+			while(j>=0 && b<=a1[j])
+			{
+				a1[j+1]=a1[j];
+				j=j-1;
+				
+			}
+			a1[j+1]=b;
+		}
+		for(int i=0;i<a1.length;i++)
+		{
+			System.out.print(a1[i]+" ");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+ 	int[] a1=new int[] {3,7,1,9,4,2,6};
+ 	insertionSort(a1);
+	}
+
+}
